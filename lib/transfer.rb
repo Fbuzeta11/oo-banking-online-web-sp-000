73d82e902db2dc0583aa517db5803b5df713cabc
Federico Buzeta Borquez <fbuzeta11@gmail.com>
2020-03-17 15:51:53 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -14,16 +14,16 @@ class Transfer
   
 
   
-  #def execute_transaction
-   # if @sender.balance > @amount && @status == "pending"
-   # @sender.balance -= @amount
-    #@receiver.balance += @amount
-    #@status = "complete"
-  #else
-   # @status = "rejected"
-    #return "Transaction rejected. Please check your account balance."
-  #end
-  #end
+  def execute_transaction
+    if @sender.balance > @amount && @status == "pending"
+    @sender.balance -= @amount
+    @receiver.balance += @amount
+    @status = "complete"
+  else
+    @status = "rejected"
+    return "Transaction rejected. Please check your account balance."
+  end
+  end
   
   def reverse_transfer
     @status = "complete"
